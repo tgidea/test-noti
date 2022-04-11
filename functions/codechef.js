64078f02cb20a8d6cb13f83f2d5ec7511d2d5fcc
Tushar <tushargoyal85709@gmail.com>
2022-04-11 07:31:39 +0530

    testing still

--- functions/codechef.js
+++ functions/codechef.js
@@ -12,61 +12,65 @@ const findDate = function (str) {
     }
 }
 const changeTime = function (str) {
-    let newTime="";
-    let start=str[0]+str[1];
-    
-    if(str[6]=='P'){
-        start=`${parseInt(start)+12}`;
+    let newTime = "";
+    let start = str[0] + str[1];
+
+    if (str[6] == 'P') {
+        start = `${parseInt(start) + 12}`;
     }
-    newTime+=start;
-    const len = str.length-7;
-    for (var i=2;i<=len;i++) {
-        newTime+=str[i];
-        if(i==4){
-            newTime+=":00";
+    newTime += start;
+    const len = str.length - 7;
+    for (var i = 2; i <= len; i++) {
+        newTime += str[i];
+        if (i == 4) {
+            newTime += ":00";
         }
-    }    
+    }
     console.log(newTime);
     return newTime;
 }
 const codechefNotification = async () => {
     try {
-        let fileResult;
+
         fs.readFile(path.join(__dirname, '../json/codechef.json'), 'utf-8', async (err, data) => {
             if (data) {
                 data = JSON.parse(data);
-                for (var k = 0; k < data.length; k++) {                    
+                for (var k = 0; k < data.length; k++) {
                     const first = data[k];
                     const time = first.time;
                     const ist = changeTime(time.toString());
                     const d = new Date();
                     const preTime = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()} ${ist}`;
                     const notifyTime = new Date(preTime);
-                                
+
                     const notifyMiliSecond = notifyTime.getTime();
-                    console.log(notifyMiliSecond,'codechef');
-                    let finalResult=notifyMiliSecond-Date.now();
-                    console.log('finalresult',finalResult)
-                    if (findDate(data[k].day.toString()) == d.getDate()  && finalResult>=(-300000) && finalResult<=1810000) { 
+
+                    let finalResult = notifyMiliSecond - Date.now();
+
+                    if (findDate(data[k].day.toString()) == d.getDate() && finalResult >= (-300000) && finalResult <= 1810000) {
                         console.log('here in codchef');
                         const result = await Subscribed.find();
-                        finalResult=finalResult-600000;
-                        if(finalResult<0){finalResult=2000};
-                        console.log('in  codechef: ',finalResult);
-                        const timeOutFunction = (result,title) => {
+                        finalResult = finalResult - 600000;
+                        if (finalResult < 0) { finalResult = 2000 };
+                        console.log('in  codechef: ', finalResult);
+                        const timeOutFunction = (result, title) => {
                             setTimeout(async () => {
                                 try {
                                     for (var i = 0; i < result.length; i++) {
-                                        const data2 = JSON.parse(result[i].subscripton);
-                                        const payload = JSON.stringify({ title: `${title}` });
-                                        const output=await webpush.sendNotification(data2, payload);
-                                        if(output){
-                                            console.log(output);
+                                        try {
+                                            const data2 = JSON.parse(result[i].subscripton);
+                                            const payload = JSON.stringify({ title: `${title}` });
+                                            const output = await webpush.sendNotification(data2, payload);
+                                            if (output) {
+                                                console.log(output);
+                                            }
+                                            else {
+                                                console.log('Erro in sending msg');
+                                            }
                                         }
-                                        else{
-                                            console.log('Erro in sending msg');
+                                        catch (err) {
+                                            console.log(err);
                                         }
-
                                     }
                                 }
                                 catch (err) {
@@ -74,7 +78,7 @@ const codechefNotification = async () => {
                                 }
                             }, finalResult);
                         }
-                        timeOutFunction(result,data[k].name);
+                        timeOutFunction(result, data[k].name);
                     }
                 }
             }
