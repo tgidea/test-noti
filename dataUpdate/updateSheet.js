ba0850cb3eebc26e98d1bd88e9b699aa79f62f79
Tushar <tushargoyal85709@gmail.com>
2022-09-15 09:11:51 +0530

    something left

--- dataUpdate/updateSheet.js
+++ dataUpdate/updateSheet.js
@@ -5,13 +5,6 @@ const rasta = path.join(__dirname, '../', 'config.env');
 require('dotenv').config({ path: rasta });
 const password = process.env.PASS;
 const url = process.env.URL;
-const updateExcel = async (name, link, time , platform) => {
-    try {
-        let payload = { name, link, time , platform , pass : "helloji" };
-                                        
-    }
-    catch (err) { console.log(err) }
-}
 
 const receiveExcel = async(aray,platform)=>{
     const obj = {
